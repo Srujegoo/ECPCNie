4afd0ec4f76f9e55cf5bf409d38a097192705ed2
Srujegoo <147052228+Srujegoo@users.noreply.github.com>
2024-05-07 21:26:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+#include <F28x_Project.h>
+//prototype statements for functions found within this file
+
+void Gpioinit(void);
+int a,b,c;
+int Array[]= {12,13,44};
+/**
+ * main.c
+ */
+int main(void)
+{
+
+	a = 24;
+	b = 67;
+
+	while(1)
+	{   Array[1] =97;
+	    c = a+b;
+
+	}
+
+}
